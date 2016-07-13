8a3f62cace97ba0596681c6d2859abc7b835c83a
Wolfgang Hoenig <whoenig@usc.edu>
2016-07-12 22:42:21 -0700

    controller tuning

--- crazyflie_manager/scripts/enablePosCtrl.py
+++ crazyflie_manager/scripts/enablePosCtrl.py
@@ -14,16 +14,17 @@ if __name__ == '__main__':
     rospy.set_param("ctrlMel/ki_xy", 0.05)
     rospy.set_param("ctrlMel/i_range_xy", 2.0)
     rospy.set_param("ctrlMel/kR_xy", 9000)
-    rospy.set_param("ctrlMel/kw_xy", 4000)
+    rospy.set_param("ctrlMel/kw_xy", 3000)
 
-    rospy.set_param("ctrlMel/kR_z", 10000)
-    rospy.set_param("ctrlMel/kw_z", 3000)
-    rospy.set_param("ctrlMel/ki_m_z", 100)
-    rospy.set_param("ctrlMel/i_range_m_z", 100)
+    rospy.set_param("ctrlMel/kR_z", 8000) # 10000
+    rospy.set_param("ctrlMel/kw_z", 3000)  # 3000
+    rospy.set_param("ctrlMel/ki_m_z", 500)     # yaw
+    rospy.set_param("ctrlMel/kd_omega_rp", 0) # roll and pitch angular velocity d gain
+    rospy.set_param("ctrlMel/i_range_m_z", 1500)
 
     rospy.set_param("ctrlMel/kp_z", 0.9)
-    rospy.set_param("ctrlMel/kd_z", 0.5)
-    rospy.set_param("ctrlMel/ki_z", 0.2)
+    rospy.set_param("ctrlMel/kd_z", 0.4) # 0.5
+    rospy.set_param("ctrlMel/ki_z", 0.2) # 0.2
     rospy.set_param("ctrlMel/i_range_z", 1.5)
 
     rospy.set_param("ctrlMel/mass", 0.032)
@@ -32,4 +33,4 @@ if __name__ == '__main__':
     rospy.set_param("ring/effect", 6)
     rospy.set_param("ring/headlightEnable", 0)
 
-    update_params(["flightmode/posCtrl", "ctrlMel/kp_xy", "ctrlMel/kd_xy", "ctrlMel/kR_xy", "ctrlMel/kw_xy", "ctrlMel/ki_xy", "ctrlMel/i_range_xy", "ctrlMel/kp_z", "ctrlMel/kd_z", "ctrlMel/ki_z", "ctrlMel/i_range_z", "ctrlMel/kR_z", "ctrlMel/kw_z", "ctrlMel/ki_m_z", "ctrlMel/i_range_m_z", "ctrlMel/mass", "ctrlMel/massThrust", "ring/effect", "ring/headlightEnable"])
+    update_params(["flightmode/posCtrl", "ctrlMel/kp_xy", "ctrlMel/kd_xy", "ctrlMel/kR_xy", "ctrlMel/kw_xy", "ctrlMel/ki_xy", "ctrlMel/i_range_xy", "ctrlMel/kp_z", "ctrlMel/kd_z", "ctrlMel/ki_z", "ctrlMel/i_range_z", "ctrlMel/kR_z", "ctrlMel/kw_z", "ctrlMel/ki_m_z", "ctrlMel/kd_omega_rp", "ctrlMel/i_range_m_z", "ctrlMel/mass", "ctrlMel/massThrust", "ring/effect", "ring/headlightEnable"])
